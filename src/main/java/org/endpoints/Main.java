e3ae8aec53af711d53365d35bdd4461da032ba38
jssancheza <juurge@gmail.com>
2023-04-23 19:03:42 +0200

    Prueba Jorge

--- src/main/java/org/endpoints/Main.java
+++ src/main/java/org/endpoints/Main.java
@@ -3,6 +3,6 @@ package org.endpoints;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Prueba PR");
+        System.out.println("Prueba Jorge");
     }
 }
