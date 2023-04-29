08e0b226953784c96e2a41fcbd50d490012ac8ff
cblas28 <cblas2001@gmail.com>
2023-04-29 10:33:58 +0200

    Commit carlos

--- src/main/java/org/endpoints/Main.java
+++ src/main/java/org/endpoints/Main.java
@@ -3,6 +3,6 @@ package org.endpoints;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Prueba Stephy ");
+        System.out.println("Prueba carlos ");
     }
 }
